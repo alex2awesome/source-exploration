3ab0cc9e7c718fc263af1bd0c028b37ff9811e38
alex2awesome <alex2awesome@gmail.com>
2019-09-22 19:38:36 -0700

    updated

--- models/ACL2013_Personas/generate_stanford_parses/run_parses.sh
+++ models/ACL2013_Personas/generate_stanford_parses/run_parses.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-datadir=../../../data/news-article-flatlist
-filelist=$datadir/filelist-to-process.txt
-ls -1 -d $datadir/raw/* > $filelist
+# get command line arguments
+while getopts ":s:e:b" opt; do
+  case $opt in
+    s) start="$OPTARG"
+    ;;
+    e) end="$OPTARG"
+    ;;
+    b) batch="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
 
-java.exe \
+datadir=../../../data/news-article-flatlist
+outputdir=$datadir/stanford-parses/$batch
+mkdir -p $outputdir
+
+### split up data
+echo 'preprocessing using python...'
+python3.7 split_a1_file.py --start $start --end $end --batch $batch
+
+## aggregate list for CoreNLP
+filelist=$datadir/filelist-to-process-batch-$batch.txt
+ls -1 -d $datadir/raw/$batch/* > $filelist
+
+echo 'running CoreNLP...'
+## run corenlp
+java \
 	-cp "../stanford-corenlp-full-2018-10-05/*" \
-	-Xmx2g \
+	-Xmx5g \
 	edu.stanford.nlp.pipeline.StanfordCoreNLP \
 	-annotators tokenize,ssplit,pos,lemma,ner,parse,coref \
 	-fileList $filelist
+	-outputDirectory $outputdir
