9b788a64ada32e2287b8ea01bdf8422649c8275b
alex2awesome <alex2awesome@gmail.com>
2019-09-22 18:40:48 -0700

    updated

--- models/ACL2013_Personas/generate_stanford_parses/run_parses.sh
+++ models/ACL2013_Personas/generate_stanford_parses/run_parses.sh
@@ -5,7 +5,8 @@ filelist=$datadir/filelist-to-process.txt
 ls -1 $datadir/raw > $filelist
 
 java.exe \
-	-cp "../java/lib" \
+	-cp "../stanford-corenlp-full-2018-10-05/*" \
+	-Xmx2g \
 	edu.stanford.nlp.pipeline.StanfordCoreNLP \
 	-annotators tokenize,ssplit,pos,lemma,ner,parse,coref \
 	-fileList $filelist
