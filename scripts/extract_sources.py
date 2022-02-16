3104e9231dae375e5f4d8a2a7eb4a398c4c55019
aspangher <aspangher@bloomberg.net>
2022-02-15 18:10:53 -0800

    e

---
+++ scripts/extract_sources.py
@@ -0,0 +1,13 @@
+import sqlite3
+import pandas as pd
+import sys
+sys.path.insert(0, '../scripts/')
+import coref_resolution_util
+
+db_path = '../data/newssniffer-nytimes.db'
+conn = sqlite3.connect(db_path)
+df = pd.read_sql('SELECT * from entryversion limit 5', con=conn)
+t = df['summary'][0].replace('</p><p>', ' ')
+
+## 
+quote_idxes, sent_words, sent_lens = coref_resolution_util.perform_quote_extraction_and_clustering(t)
